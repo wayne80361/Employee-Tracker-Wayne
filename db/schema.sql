d962fa1ea77d31762d760eac77ee93aaaf7c3ae5
wayne80361 <wayne80361@gmail.com>
2023-10-02 23:02:29 -0400

    add schema.sql with businesses_db, with department, role, and employee table, require inquirer in server.js

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS businesses_db;
+CREATE DATABASE businesses_db;
+
+USE businesses_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
