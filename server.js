c0bf44f05f751fdcdafb9a74b2cfb0c385e341f4
wayne80361 <wayne80361@gmail.com>
2023-10-04 02:06:54 -0400

    add manager/role selection questions, and function to retrive existing employee/role to the choice

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 
 const { addEmployee } = require("./employeeFunction.js");
 const { addRole } = require("./roleFunction.js");
+const connection = require("./config/connection.js");
 
 const employeeQuestions = [
   {
@@ -14,7 +15,19 @@ const employeeQuestions = [
     name: "last_name",
     message: "Enter the last name of the employee:",
   },
-  // Add more questions for role and manager selection
+  {
+    type: "list",
+    name: "manager_id",
+    message: 'Select the employee\'s manager (or choose "None"):',
+    choices: [],
+  },
+  {
+    type: "list",
+    name: "role_id",
+    message: "Select the employee's role:",
+    choices: [],
+  },
+  // Add more questions for role selection if needed
 ];
 
 const roleQuestions = [
@@ -31,23 +44,84 @@ const roleQuestions = [
   // Add more questions as needed
 ];
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "action",
-      message: "What would you like to do?",
-      choices: ["Add Employee", "Add Role"],
-    },
-  ])
-  .then((answers) => {
-    if (answers.action === "Add Employee") {
-      inquirer.prompt(employeeQuestions).then((employeeData) => {
-        addEmployee(employeeData);
-      });
-    } else if (answers.action === "Add Role") {
-      inquirer.prompt(roleQuestions).then((roleData) => {
-        addRole(roleData);
-      });
-    }
+const getRoleList = () => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT id, title FROM role", (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        // map the list of existing roles for selection
+        const roleList = results.map((role) => ({
+          name: role.title,
+          value: role.id,
+        }));
+        resolve(roleList);
+      }
+    });
   });
+};
+
+const getEmployeeList = () => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      'SELECT id, CONCAT(first_name, " ", last_name) AS full_name FROM employee',
+      (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          // map the list of existing employees for manager selection
+          const employeeList = results.map((employee) => ({
+            name: employee.full_name,
+            value: employee.id,
+          }));
+          // Add the "None" option to the list of employees
+          employeeList.unshift({ name: "None", value: null });
+          resolve(employeeList);
+        }
+      }
+    );
+  });
+};
+
+const init = async () => {
+  // Get the list of existing employees for manager selection
+  const employeeList = await getEmployeeList();
+  employeeQuestions[2].choices = employeeList;
+  // Add the employee choices to the employeeQuestions
+  //   employeeQuestions[2] = {
+  //     type: "list",
+  //     name: "manager_id",
+  //     message: 'Select the employee\'s manager (or choose "None"):',
+  //     choices: employeeList,
+  //   };
+  const roleList = await getRoleList();
+  employeeQuestions[3].choices = roleList;
+  //   employeeQuestions[3] = {
+  //     type: "list",
+  //     name: "role_id",
+  //     message: "Select the employee's role:",
+  //     choices: roleList,
+  //   };
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: ["Add Employee", "Add Role"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.action === "Add Employee") {
+        inquirer.prompt(employeeQuestions).then((employeeData) => {
+          addEmployee(employeeData);
+        });
+      } else if (answers.action === "Add Role") {
+        inquirer.prompt(roleQuestions).then((roleData) => {
+          addRole(roleData);
+        });
+      }
+    });
+};
+init();
