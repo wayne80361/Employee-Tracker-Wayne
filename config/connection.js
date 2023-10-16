7a23829f6e99bee4f10ab638a9853458e779c017
wayne80361 <wayne80361@gmail.com>
2023-10-16 01:36:27 -0400

    add .env npm package

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,15 @@ const mysql = require("mysql2");
 const cyanColor = "\x1b[36m";
 const greenColor = "\x1b[32m";
 const whiteColor = "\x1b[37m";
+require("dotenv").config();
 
 // create connection to database
 const connection = mysql.createConnection(
   {
     host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "businesses_db",
+    user: process.env.user,
+    password: process.env.pass,
+    database: process.env.db_name,
   },
   console.log(
     `${cyanColor}Connected to the ${greenColor}businesses_db ${cyanColor}database${whiteColor}`
